a6223173bd467b75b9e2dc16a2bf69162940fa77
FilmyKannadiga <naveengowdathegreat@gmail.com>
2026-03-03 20:49:28 +0530

    Update info.py

--- info.py
+++ info.py
@@ -108,11 +108,11 @@ TUTORIAL_3 = environ.get("TUTORIAL_3", "https://t.me/dreamxbotz")   # Third tuto
 SHORTENER_API = environ.get("SHORTENER_API", "2881ed595d04aa3494123b92cf03dd20633eb015") # Shortener API key
 SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", "mdiskshort.in") # Shortener website
 
-SHORTENER_API2 = environ.get("SHORTENER_API2", "")  # Shortener API key for second website
-SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", "") # Shortener website for second website
+SHORTENER_API2 = environ.get("SHORTENER_API2", "2881ed595d04aa3494123b92cf03dd20633eb015")  # Shortener API key for second website
+SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", "mdiskshort.in") # Shortener website for second website
 
-SHORTENER_API3 = environ.get("SHORTENER_API3", "")  
-SHORTENER_WEBSITE3 = environ.get("SHORTENER_WEBSITE3", "") # Shortener website for third website
+SHORTENER_API3 = environ.get("SHORTENER_API3", "2881ed595d04aa3494123b92cf03dd20633eb015")  
+SHORTENER_WEBSITE3 = environ.get("SHORTENER_WEBSITE3", "mdiskshort.in") # Shortener website for third website
 
 TWO_VERIFY_GAP = int(environ.get('TWO_VERIFY_GAP', "1200")) # Time gap for two-step verification in seconds (default: 20 minutes)
 THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "54000"))    
